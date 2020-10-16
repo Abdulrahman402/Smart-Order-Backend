8fe422a037c1e8c3577c6678b4bb28829e70abda
Abdulrahman <abduwemoh@gmail.com>
2020-10-16 14:23:41 +0200

    user APIs added

--- Routes/user.js
+++ Routes/user.js
@@ -2,11 +2,31 @@ const express = require("express");
 
 const router = express.Router();
 
-const addAdmin = require("../Controllers/User/addAdmin");
-const logIn = require("../Controllers/User/logIn");
+const auth = require("../Middleware/auth");
 
-router.post("/addAdmin", addAdmin.addAdmin);
+const addAdmin = require("../Controllers/User/addAdmin");
+const addCashier = require("../Controllers/User/addCashier");
+const logIn = require("../Controllers/User/logIn");
+const allAdmins = require("../Controllers/User/allAdmins");
+const allCashiers = require("../Controllers/User/allCashiers");
+const myProfile = require("../Controllers/User/myProfile");
+const changeName = require("../Controllers/User/changeName");
+const changePassword = require("../Controllers/User/changePassword");
+
+router.post("/addAdmin", auth, addAdmin.addAdmin);
+
+router.post("/addCashier", auth, addCashier.addCashier);
 
 router.post("/logIn", logIn.logIn);
+
+router.get("/allAdmins", auth, allAdmins.allAdmins);
+
+router.get("/allCashiers", auth, allCashiers.allCashiers);
+
+router.get("/myProfile", auth, myProfile.myProfile);
+
+router.put("/changeName", auth, changeName.changeName);
+
+router.put("/changePassword", auth, changePassword.changePassword);
 
 module.exports = router;
