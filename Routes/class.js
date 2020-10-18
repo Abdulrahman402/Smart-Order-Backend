f5d6373fb613991cb646d8cfdbd870e3309fd63a
Abdulrahman <abduwemoh@gmail.com>
2020-10-18 17:20:06 +0200

    Class APIs created

--- Routes/class.js
+++ Routes/class.js
@@ -4,9 +4,15 @@ const router = express.Router();
 
 const addClass = require("../Controllers/Class/addClass");
 const showClass = require("../Controllers/Class/showClass");
+const updateName = require("../Controllers/Class/updateName");
+const removeClass = require("../Controllers/Class/removeClass");
 
 router.post("/addClass", addClass.addClass);
 
+router.put("/updateName/:classId", updateName.updateName);
+
 router.get("/showClass/:englishName", showClass.showClass);
+
+router.delete("/removeClass/:classId", removeClass.removeClass);
 
 module.exports = router;
